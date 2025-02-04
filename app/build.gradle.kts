671f6b90a1cd6b8279584630f5a7f0decc743e37
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-04 15:08:55 -0300

    Criação e configuração inicial do projeto

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+    kotlin("kapt")
+}
+
+android {
+    namespace = "br.edu.ifsp.dmo.financeapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.edu.ifsp.dmo.financeapp"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    val version = "2.6.1"
+    implementation("androidx.room:room-runtime:$version")
+    implementation("androidx.room:room-ktx:$version")
+    kapt("androidx.room:room-compiler:$version")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
