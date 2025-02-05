b6688cfbec21bb1346cf829c64d9b80f40182c3c
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-05 15:52:22 -0300

    Criação da tela inicial

--- app/src/main/java/br/edu/ifsp/dmo/financeapp/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/financeapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package br.edu.ifsp.dmo.financeapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Layout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo.financeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setCardClick()
+    }
+
+    private fun setCardClick() {
+        binding.buyCard.setOnClickListener {
+            Toast.makeText(this, "Buy Card", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.checkExpensesCard.setOnClickListener {
+            Toast.makeText(this, "Check Expenses", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.financialGoalsCard.setOnClickListener {
+            Toast.makeText(this, "Financial Goals Card", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.fixedBillsCard.setOnClickListener() {
+            Toast.makeText(this, "Fixed Bills Card", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
