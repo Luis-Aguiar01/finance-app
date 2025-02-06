eb4ab12c69689dc7c3afbe45a258b19094caa0b2
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-06 15:11:19 -0300

    Criação da página inicial o aplicativo

---
+++ app/src/main/java/br/edu/ifsp/dmo/financeapp/InitialActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.dmo.financeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InitialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
