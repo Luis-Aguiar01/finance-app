2e58bd52926ef3ca98b906009acef24d43fb872d
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-06 10:23:35 -0300

    Criação da actvity de metas

---
+++ app/src/main/java/br/edu/ifsp/dmo/financeapp/GoalsActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.dmo.financeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_goals)
+    }
+}
