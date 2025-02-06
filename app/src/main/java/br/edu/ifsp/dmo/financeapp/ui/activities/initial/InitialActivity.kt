79f0465382ce31ad9772783ae94959afba788ca0
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-06 18:35:26 -0300

    Adicionando navegação na tela inicial

--- app/src/main/java/br/edu/ifsp/dmo/financeapp/ui/activities/initial/InitialActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/financeapp/ui/activities/initial/InitialActivity.kt
@@ -1,12 +1,31 @@
 package br.edu.ifsp.dmo.financeapp.ui.activities.initial
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import br.edu.ifsp.dmo.financeapp.R
+import br.edu.ifsp.dmo.financeapp.databinding.ActivityInitialBinding
+import br.edu.ifsp.dmo.financeapp.ui.activities.login.LoginActivity
+import br.edu.ifsp.dmo.financeapp.ui.activities.register.RegisterActivity
 
 class InitialActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInitialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial)
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configListeners()
+    }
+
+    private fun configListeners() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.registerButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
