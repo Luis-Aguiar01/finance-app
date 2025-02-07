d6fe5234891ca4b7858370f3d8a069d617f841d8
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-07 16:05:31 -0300

    Criação do design do dialog de adição de metas

--- app/src/main/java/br/edu/ifsp/dmo/financeapp/ui/activities/goals/GoalsActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/financeapp/ui/activities/goals/GoalsActivity.kt
@@ -1,8 +1,12 @@
 package br.edu.ifsp.dmo.financeapp.ui.activities.goals
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.financeapp.R
 import br.edu.ifsp.dmo.financeapp.databinding.ActivityGoalsBinding
+import br.edu.ifsp.dmo.financeapp.databinding.LayoutDialogGoalBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class GoalsActivity : AppCompatActivity() {
 
@@ -18,6 +22,33 @@ class GoalsActivity : AppCompatActivity() {
     private fun configListeners() {
         binding.arrowBack.setOnClickListener {
             finish()
+        }
+
+        binding.addButton.setOnClickListener {
+            val dialogBinding = LayoutDialogGoalBinding.inflate(layoutInflater)
+
+            val dialog = MaterialAlertDialogBuilder(this, R.style.CustomDialogTheme)
+                .setView(dialogBinding.root)
+                .show()
+
+            dialogBinding.confirmButton.setOnClickListener {
+                val product = dialogBinding.inputProduct.text.toString()
+                val initialAmount = dialogBinding.inputCurrentAmount.text.toString()
+                val finalAmount = dialogBinding.inputFinalAmount.text.toString()
+
+                if (product.isNotEmpty() && initialAmount.isNotEmpty() && finalAmount.isNotEmpty()) {
+                    val dInitialAmount = initialAmount.toDouble()
+                    val dFinalAmount = finalAmount.toDouble()
+                    dialog.dismiss()
+                    Toast.makeText(this, "Product: $product, Initial Amount: R$$dInitialAmount, Final Amount: R$$dFinalAmount", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Adicione todas as informações do produto.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            dialogBinding.cancelButton.setOnClickListener {
+                dialog.dismiss()
+            }
         }
     }
 }
